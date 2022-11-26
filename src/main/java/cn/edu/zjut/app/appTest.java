ccded40e9b8e77ebbbdb96fabdadc10512b1fd50
何 桐 <xhtxzc@hotmail.com>
2022-11-26 20:14:33 +0800

    邮箱工具类

---
+++ src/main/java/cn/edu/zjut/app/appTest.java
@@ -0,0 +1,8 @@
+package cn.edu.zjut.app;
+import cn.edu.zjut.util.*;
+
+public class appTest {
+    public static void main(String[] args) {
+        MailUtil.sendMail("xhtxzc@hotmail.com","22","22");
+    }
+}
