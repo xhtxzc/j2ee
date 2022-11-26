30367a38489d08ca98c98b689abcc11e79442731
何 桐 <xhtxzc@hotmail.com>
2022-11-26 19:21:58 +0800

    1

---
+++ src/main/java/cn/edu/zjut/pojo/Test.java
@@ -0,0 +1,4 @@
+package cn.edu.zjut.pojo;
+
+public class Test {
+}
---
+++ src/test/java/cn/edu/zjut/app/J2eeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2eeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
